bfeca65b061d3fc9c2268e3649b4ede841ff277e
Moges741 <mogesse741@gmail.com>
2026-07-13 09:04:34 +0300

    render ai widget

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Toaster } from 'react-hot-toast'
 import AppRouter from './router/AppRouter'
 import SessionInitializer from './features/auth/SessionInitializer'
+import AiAssistantWidget from './features/ai/AiAssistantWidget'
 
 function App() {
   return (
     <SessionInitializer>
       <AppRouter />
+      <AiAssistantWidget />
       <Toaster position="top-center" />
     </SessionInitializer>
   )
