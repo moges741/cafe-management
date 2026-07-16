980a1b9cd3bc28a4b9aadf4abe63c9a7b0d884ce
Moges741 <mogesse741@gmail.com>
2026-07-16 20:36:01 +0300

    added home page as a wrapper

---
+++ src/components/home/HomePage.tsx
@@ -0,0 +1,41 @@
+import Navbar from '@/components/layout/Navbar'
+import Footer from '@/components/layout/Footer'
+import HeroSection from '@/components/home/HeroSection'
+import StatsSection from '@/components/home/StatsSection'
+import AboutSection from '@/components/home/AboutSection'
+import WhyChooseUsSection from '@/components/home/WhyChooseUsSection'
+import FeaturedMenuSection from '@/components/home/FeaturedMenuSection'
+import AiFeaturesSection from '@/components/home/AiFeaturesSection'
+import CustomerFavoritesSection from '@/components/home/CustomerFavoritesSection'
+import GallerySection from '@/components/home/GallerySection'
+import TestimonialsSection from '@/components/home/TestimonialsSection'
+import ReservationSection from '@/components/home/ReservationSection'
+import EventsSection from '@/components/home/EventsSection'
+import TeamSection from '@/components/home/TeamSection'
+import NewsletterSection from '@/components/home/NewsletterSection'
+// import LocationSection from '@/components/home/LocationSection'
+import FaqSection from '@/components/home/FaqSection'
+
+export default function HomePage() {
+  return (
+    <div className="bg-background">
+      <Navbar />
+      <HeroSection />
+      <StatsSection />
+      <AboutSection />
+      <WhyChooseUsSection />
+      <FeaturedMenuSection />
+      <AiFeaturesSection />
+      <CustomerFavoritesSection />
+      <GallerySection />
+      <TestimonialsSection />
+      <ReservationSection />
+      <EventsSection />
+      <TeamSection />
+      <NewsletterSection />
+      {/* <LocationSection /> */}
+      <FaqSection />
+      <Footer />
+    </div>
+  )
+}
